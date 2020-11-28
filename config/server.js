2ecaaa06aea35d73b1d3f539dd27bd6af9dc9ed3
Edgar Lopez <edgarlopezmgs@live.com>
2020-11-27 22:59:24 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15da9cf13facaa28b322de7ac42923fd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4aa9b1a2-c52b-453a-a6f6-42892225eadc'
+};
